b5f097e2b82b2d7d856271e0254f514890c473a2
Long Nguyen <zakelong81@yahoo.com>
2017-11-09 22:00:40 -0800

    fixing bug c

--- stocks/urls.py
+++ stocks/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
         url(r'^$', views.index, name='index'),
+        url(r'^index/', views.index, name='index'),
+        url(r'^signup/', views.signup, name='signup'),
+        url(r'^login/', views.login, name='login'),
+
     ]
--- stocks/views.py
+++ stocks/views.py
@@ -5,3 +5,11 @@ from django.shortcuts import render
 def index(request):
     context = {'Title':"Title"}
     return render(request, "index.html", context)
+
+def signup(request):
+    context = {'Title':"Title"}
+    return render(request, "signup.html", context)
+
+def login(request):
+    context = {'Title':"Title"}
+    return render(request, "login.html", context)
